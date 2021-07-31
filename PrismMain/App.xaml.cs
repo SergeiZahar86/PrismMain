3b30d72d353cd7e7f9fe8ba295faf43859378dca
Сергей Захаров <sergei86zahar@mail.ru>
2021-07-31 22:22:31 +0700

    Добавьте файлы проекта.

---
+++ PrismMain/App.xaml.cs
@@ -0,0 +1,26 @@
+﻿using Prism.Ioc;
+using Prism.Modularity;
+using PrismMain.Views;
+using System.Windows;
+
+namespace PrismMain
+{
+    public partial class App
+    {
+        protected override Window CreateShell()
+        {
+            return Container.Resolve<MainWindow>();
+        }
+
+        protected override void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+
+        }
+
+        protected override void ConfigureModuleCatalog(IModuleCatalog moduleCatalog)
+        {
+            //moduleCatalog.AddModule<MailModule>();
+            //moduleCatalog.AddModule<ContactsModule>();
+        }
+    }
+}
---
+++ PrismMain/Views/MainWindow.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Windows;
+
+namespace PrismMain.Views
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void PresetTimePicker_SelectedTimeChanged(object sender, RoutedPropertyChangedEventArgs<DateTime?> e)
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+}
